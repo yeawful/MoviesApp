63c4e8188ebde2375ba0089b55b497bb2cdd8741
yeawful <paketa1337@gmail.com>
2025-05-02 14:46:43 +0300

    fix ui bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App/App.jsx';
+import 'normalize.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
